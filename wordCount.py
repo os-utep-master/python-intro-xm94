d8483ba584db0e74497269e5fb5feaa59cf71220
xm94 <xavier_martinez94@yahoo.com>
2019-09-01 23:39:04 -0600

    fixed wordCount.py so it works in Python 3 and it fits the execution style in wordCountTest.py, now will pass without having to be run first

--- wordCount.py
+++ wordCount.py
@@ -3,16 +3,17 @@ import re
 import string
 
 def main():
-    if len(sys.argv) is not 2:
-        print("Correct usage: wordCount.py <input text file>")
+    if len(sys.argv) is not 3:
+        print("Correct usage: wordCount.py <input text file> <output text file>")
         exit()
 
     inputText = sys.argv[1]
+    outputText = sys.argv[2]#a string ending in .txt that will be output to
     wordCounts={}#dictionary containing the counts
     with open(inputText,'r') as inputFile:
         for line in inputFile:
-            line = line.strip()
-            noPunct = string.maketrans(string.punctuation, ' '*len(string.punctuation))
+            line = line.strip()#removes newlines
+            noPunct = str.maketrans(string.punctuation, ' '*len(string.punctuation))#translates punctuation to spaces
             line = line.translate(noPunct)#removes punctuatuion
             line = line.lower()#changing to all lowercase
             words = re.split('[ \t]', line)#splitting into individual words
@@ -23,11 +24,10 @@ def main():
                     if word != "":#putting in because the blank character kept showing up
                         wordCounts[word]=1
         inputFile.close()
-    sortedKeys = sorted(wordCounts.iterkeys())#sorts the keys
-    f = open("output.txt",'w+');#output file named output.txt
-    for keys in sortedKeys:
+    sortedKeys = sorted(iter(wordCounts.keys()))#sorts the keys
+    f = open(outputText,'w+');#output file named output.txt
+    for keys in sortedKeys:#loops through the dictionary in alphabetical order
         f.write(keys + " " + str(wordCounts[keys]) + "\n")
-
     f.close();
 
 if __name__ =='__main__':
