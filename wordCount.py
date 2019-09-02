4da194fba8b94b352e2ef6cd72e9ac76a09b96a2
xm94 <xavier_martinez94@yahoo.com>
2019-09-01 23:11:43 -0600

    adding what I have, currently failing on declaration

---
+++ wordCount.py
@@ -0,0 +1,30 @@
+import sys
+import re
+import string
+
+def main():
+    o = "output"
+    wordCounts={}#dictionary containing the counts
+    with open("declaration.txt",'r') as inputFile:
+        for line in inputFile:
+            line = line.strip()
+            line = line.translate(None, string.punctuation)#removes punctuatuion
+            line = line.lower()#changing to all lowercase
+            words = re.split('[ \t]', line)#splitting into individual words
+            for word in words:
+                if word in wordCounts:#adds to the count
+                    wordCounts[word]+=1
+                else:#adds to the dictionary and sets it to 1
+                    if word != "":#putting in because the blank character kept showing up
+                        wordCounts[word]=1
+            print "\n this is a new line\n"
+        inputFile.close()
+    sortedKeys = sorted(wordCounts.iterkeys())#sorts the keys
+    f = open(o + ".txt",'w+');
+    for keys in sortedKeys:
+        f.write(keys + " " + str(wordCounts[keys]) + "\n")
+
+    f.close();
+
+if __name__ =='__main__':
+    main()
